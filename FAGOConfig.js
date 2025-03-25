f8153dca1ba4570614f84d97ea2f9f85144f0d43
Mimukauwa <solavolodimir@gmail.com>
2025-03-25 18:51:31 +0100

    Create FAGOConfig.js
    Signed-off-by: Mimukauwa <solavolodimir@gmail.com>

---
+++ FAGOConfig.js
@@ -0,0 +1,6 @@
+// Configurable values for FAGO
+export const STARTING_BLOCKS = 200;
+export const LOADING_SCREEN_ASSET = "/lv_0_20250325170945.gif";
+export const BLOCK_SIZE = 1;
+export const GRID_SIZE = 20;
+export const AUTOSAVE_INTERVAL = 600000; // 10 minutes in milliseconds
